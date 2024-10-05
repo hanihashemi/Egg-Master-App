e7cc4ed3f4c4ba26b755aff76258e5b693f1a1cf
Hani <jhanihashemi@gmail.com>
2024-10-05 15:47:56 +0200

    Fix SplashScreen bottom bar button

--- app/src/main/java/io/github/hanihashemi/eggmaster/splash/SplashScreen.kt
+++ app/src/main/java/io/github/hanihashemi/eggmaster/splash/SplashScreen.kt
@@ -1,6 +1,7 @@
 package io.github.hanihashemi.eggmaster.splash
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,73 +9,102 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.github.hanihashemi.eggmaster.components.Button
+import io.github.hanihashemi.eggmaster.components.ButtonDefaultStyles
+import io.github.hanihashemi.eggmaster.ui.theme.Dimens
+import io.github.hanihashemi.eggmaster.ui.theme.EggMasterTheme
+import io.github.hanihashemi.eggmaster.ui.theme.Shapes
 
 
 @Composable
 fun SplashScreen(onStartClick: () -> Unit) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Column {
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomBar { onStartClick() } },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(paddingValues)
+                .padding(Dimens.PaddingNormal),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
             Box(
-                modifier = Modifier
-                    .weight(1F)
-                    .fillMaxSize()
-                    .padding(16.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        "Egg\n master",
-                        lineHeight = 45.sp,
-                        style = MaterialTheme.typography.displayMedium
-                    )
-                    Text(
-                        "prepare eggs as you like!",
-                        style = MaterialTheme.typography.bodyLarge.merge(
-                            TextStyle(
-                                color = Color.White.copy(alpha = 0.4F)
-                            )
-                        )
-                    )
-                }
-
-            }
-            Box(
-                modifier = Modifier
-                    .weight(2F)
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
             ) {
                 EggLogoComponent()
             }
-            Box(
-                modifier = Modifier
-                    .weight(1F)
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                OutlinedButton(
-                    modifier = Modifier
-                        .widthIn(max = 250.dp)
-                        .fillMaxWidth(),
-                    border = BorderStroke(2.dp, Color.White),
-                    onClick = onStartClick
-                ) {
-                    Text(text = "Let's start", color = Color.White)
-                }
-            }
-
         }
+    }
+}
+
+@Composable
+private fun TopBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = Dimens.PaddingXLarge),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Egg\n master",
+                lineHeight = 45.sp,
+                style = MaterialTheme.typography.displayMedium
+            )
+            Text(
+                text = "prepare eggs as you like!",
+                style = MaterialTheme.typography.bodyLarge.merge(
+                    TextStyle(
+                        color = Color.White.copy(alpha = 0.4F)
+                    )
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(onStartClick: () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .widthIn(max = 350.dp)
+                .padding(Dimens.PaddingNormal)
+                .clip(Shapes.large)
+                .fillMaxWidth()
+                .padding(Dimens.PaddingLarge),
+            contentAlignment = Alignment.Center,
+        ) {
+            Button(
+                text = "Let's start",
+                onClick = { onStartClick() },
+                style = ButtonDefaultStyles.Secondary,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    EggMasterTheme {
+        SplashScreen(onStartClick = {})
     }
 }
