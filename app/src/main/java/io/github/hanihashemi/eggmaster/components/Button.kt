2a76957b542c10c7cf19327ea4a6d86276733e3a
Hani <jhanihashemi@gmail.com>
2024-12-24 16:47:07 +0100

    Add Outline and Transparent button styles

--- app/src/main/java/io/github/hanihashemi/eggmaster/components/Button.kt
+++ app/src/main/java/io/github/hanihashemi/eggmaster/components/Button.kt
@@ -1,31 +1,39 @@
 package io.github.hanihashemi.eggmaster.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import io.github.hanihashemi.eggmaster.ui.theme.Dimens
 import io.github.hanihashemi.eggmaster.ui.theme.Shapes
 import io.github.hanihashemi.eggmaster.ui.theme.Typography
 
 @Composable
 fun Button(
+    modifier: Modifier = Modifier,
     text: String,
     style: ButtonStyle = ButtonDefaultStyles.Primary,
     onClick: () -> Unit = {},
 ) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .clip(Shapes.large)
+            .border(BorderStroke(style.borderWidth, style.borderColor), CircleShape)
             .background(color = style.backgroundColor)
             .clickable { onClick() }
             .padding(Dimens.PaddingNormal),
@@ -34,7 +42,7 @@ fun Button(
         Text(
             text = text,
             color = style.textColor,
-            style = Typography.displaySmall, // TODO: use Theme
+            style = style.textStyle,
         )
     }
 }
@@ -50,11 +58,26 @@ object ButtonDefaultStyles {
         textColor = Color(0xFF2C2D5C),
     )
 
+    val Outline = ButtonStyle(
+        backgroundColor = Color(0x00E5E5E5),
+        textColor = Color(0xFFFFFFFF),
+        borderColor = Color(0xFFFFFFFF),
+        borderWidth = 2.dp,
+    )
+
+    val Transparent = ButtonStyle(
+        backgroundColor = Color.Transparent,
+        textColor = Color(0xFF949FDA),
+        textStyle = Typography.bodySmall,
+    )
 }
 
 data class ButtonStyle(
     val backgroundColor: Color,
     val textColor: Color,
+    val borderColor: Color = Color.Transparent,
+    val borderWidth: Dp = 0.dp,
+    val textStyle: TextStyle = Typography.displaySmall,
 )
 
 @Preview
@@ -69,5 +92,23 @@ fun ButtonSecondaryPreview() {
     Button(
         text = "Next",
         style = ButtonDefaultStyles.Secondary
+    )
+}
+
+@Preview
+@Composable
+fun OutlineButtonPrimaryPreview() {
+    Button(
+        text = "Next",
+        style = ButtonDefaultStyles.Outline
+    )
+}
+
+@Preview
+@Composable
+fun TransparentButtonPrimaryPreview() {
+    Button(
+        text = "Next",
+        style = ButtonDefaultStyles.Transparent
     )
 }
