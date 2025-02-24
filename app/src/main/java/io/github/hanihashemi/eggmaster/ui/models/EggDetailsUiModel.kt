aeea7b6980716cb5f08841fffe73ee9777a3266b
Hani <jhanihashemi@gmail.com>
2025-02-24 17:04:51 +0100

    Add egg timer mode to egg details model.

--- app/src/main/java/io/github/hanihashemi/eggmaster/ui/models/EggDetailsUiModel.kt
+++ app/src/main/java/io/github/hanihashemi/eggmaster/ui/models/EggDetailsUiModel.kt
@@ -3,9 +3,9 @@ package io.github.hanihashemi.eggmaster.ui.models
 data class EggDetailsUiModel(
     val temperature: EggTemperature = EggTemperature.FRIDGE,
     val size: EggSize = EggSize.Medium,
-    val count: Int = 1,
     val boiledType: EggBoiledType = EggBoiledType.MEDIUM,
     val boilingTime: Int = 360,
+    val isEggTimerMode: Boolean = true,
 )
 
 enum class EggTemperature {
